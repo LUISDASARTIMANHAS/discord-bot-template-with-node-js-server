81a89df8dc9fdea2e6c5600bb9e1cda2820d2c0a
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-10-07 22:00:08 -0300

    updated

--- modules/autoFileSysModule.js
+++ modules/autoFileSysModule.js
@@ -1,4 +1,5 @@
-import fs from "fs";
+const fs = require("fs");
+const path = require("path");
 
 function fopen(filePath) {
   const database = fs.readFileSync(filePath, "utf8");
@@ -8,9 +9,43 @@ function fopen(filePath) {
 }
 
 function fwrite(filePath, data) {
-  const formatData = JSON.stringify(data, null, 2)
+  const formatData = JSON.stringify(data, null, 2);
+
   fs.writeFileSync(filePath, formatData, "utf8");
   return true;
 }
 
-export { fopen, fwrite };
+// Função para converter uma string em uma representação binária
+function stringToBinary(str) {
+  return str
+    .split("")
+    .map((char) => {
+      return char.charCodeAt(0).toString(2).padStart(8, "0");
+    })
+    .join(" ");
+}
+
+// Função para salvar dados JSON como binário
+function fwriteBin(filePath, data) {
+  const jsonString = JSON.stringify(data);
+  const binaryString = stringToBinary(jsonString);
+
+  fs.writeFileSync(filePath, binaryString, "utf8");
+  return true;
+}
+
+// Leitura e conversão de volta para JSON
+function freadBin(filePath) {
+  const binaryString = fs.readFileSync(filePath, "utf8");
+  const jsonString = binaryString
+    .split(" ")
+    .map((bin) => {
+      return String.fromCharCode(parseInt(bin, 2));
+    })
+    .join("");
+  const data = JSON.parse(jsonString);
+
+  return data;
+}
+
+module.exports = { fopen, fwrite, freadBin, fwriteBin, saveFile};
