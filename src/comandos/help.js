a6d2873fdcdfb95a1148361204448bdeeadfd655
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-10-07 22:39:10 -0300

    updated default command

--- src/comandos/help.js
+++ src/comandos/help.js
@@ -1,36 +1,37 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 
 let helpCommand = new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Usado para ver os comandos e informações')
+  .setName("help")
+  .setDescription("Usado para ver os comandos e informações");
 
 helpCommand = helpCommand.toJSON();
 
 // help.js
 function handleHelp(interaction) {
-    if (interaction.commandName === 'help') {
-        const novaLinha = "\n";
-        let info = "ℹ️__**prefixo:**__ " + prefix + novaLinha;
+  if (interaction.commandName === "help") {
+    const date = new Date();
+    const ano = date.getFullYear();
+    let info = "";
+    let comandos = [{ title: "a", description: "a" }];
+    // Iterar sobre cada comando no JSON
+    comandos.forEach((comando) => {
+      info += `__**${comando.title}**__
+							${comando.description}
+							`;
+    });
 
-        // Iterar sobre cada comando no JSON
-        comandos.forEach((comando) => {
-            info += "__**" + comando.title + "**__" +
-                novaLinha +
-                comando.description +
-                novaLinha
-        });
+    const embed = {
+      title: "**__PAINEL DE AJUDA__**",
+      description: info,
+      color: parseInt("FF00FF", 16),
+      timestamp: date, // Adiciona um timestamp atual
+      footer: {
+        text: `₢Todos os Direitos Reservados - ${ano}`,
+      },
+    };
 
-        const embed = new Discord.MessageEmbed();
-        embed.setTitle(`**__PAINEL DE AJUDA__**`);
-        embed.setColor("#FF00FF");
-        embed.setDescription(info);
-        embed.setTimestamp();
-
-        interaction.reply({ embeds: [embed] });
-    }
+    interaction.reply({ embeds: [embed] });
+  }
 }
 
-export {
-    helpCommand,
-    handleHelp
-}
+export { helpCommand, handleHelp };
