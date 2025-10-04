db10e07c90e8323e9ed0b0b9655b61217587f24d
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-10-04 11:05:52 -0300

    fixed legacy itens

--- pages.js
+++ pages.js
@@ -1,28 +1,66 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
-import path from "path";
-import {fileURLToPath} from 'url';
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const {
+  getRandomInt,
+  getRandomBin,
+  getRandomHex,
+  generateToken,
+  ordenarUsuario,
+  pesqUsuario,
+  validadeApiKey,
+  unauthorized,
+  forbidden,
+  formatDate,
+  conversorSimEnao,
+  landingPage
+} = require("npm-package-nodejs-utils-lda");
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const files = __dirname + "/src/";
 const path_css = files + "css/";
-const path_js = files + "js/"
+const path_js = files + "js/";
 const path_pages = files + "pages/";
 const indexFilePath = path.join(path_pages, "index.html");
+const hostFilePath = path.join(path_pages, "host.html");
 
 router.use(express.static(files));
 console.log("LOAD STATIC ITENS: " + path_css);
 console.log("LOAD STATIC ITENS: " + path_js);
 console.log("LOAD STATIC ITENS: " + path_pages);
 
-
 router.get("/", (req, res) => {
   console.log("SISTEMA <OBTER> <SITE>: " + req.url);
-  res.sendFile(indexFilePath);
+  landingPage(res);
+});
+
+router.get('/status', (req, res) => {
+  const healthCheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now(),
+    cpuUsage: os.loadavg(),
+    memoryUsage: process.memoryUsage(),
+  };
+
+  try {
+    res.json(healthCheck);
+  } catch (e) {
+    healthCheck.message = e;
+    res.status(503).send();
+  }
+});
+
+
+router.get("/host", (req, res) => {
+  console.log("SISTEMA <OBTER> <SITE>: " + req.url);
+  res.sendFile(hostFilePath);
 });
 
 router.get("/debugger", (req, res) => {
   console.log("SISTEMA <OBTER> <SITE>: " + req.url);
-  res.status(200)
+  res.status(200);
 });
-export default router;
+
+module.exports = router;
--- rotas.js
+++ rotas.js
@@ -1,27 +1,25 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
-import path from "path";
-import {fileURLToPath} from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const files2 = __dirname + "/src/";
-const path_pages = files2 + "pages/";
-const forbiddenFilePath = path.join(path_pages, "forbidden.html");
-const notFoundFilePath = path.join(path_pages, "not-found.html");
-
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
-
-// Rota blackhole para lidar com muitas requisições
-router.use('/blackhole', (req, res) => {
-  res.status(429);
-  res.send('Too Many Requests // Muitas Solicitações!');
-});
+const {
+  fetchGet,
+  fetchPost,
+  notfound,
+  getRandomInt,
+  getRandomBin,
+  getRandomHex,
+  generateToken,
+  ordenarUsuario,
+  pesqUsuario,
+  validadeApiKey,
+  unauthorized,
+  forbidden,
+  formatDate,
+  conversorSimEnao,
+} = require("npm-package-nodejs-utils-lda");
 
 // Middleware para lidar com rotas não encontradas (404)
 router.use((req, res, next) => {
-  res.status(404);
-  res.sendFile(notFoundFilePath);
+  notfound(res);
 });
 
-export default router;
+module.exports = router;
