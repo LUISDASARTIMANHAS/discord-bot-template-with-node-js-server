0737a1d25b5b43029cdf1500c438984abe074859
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-11-01 16:35:01 -0300

    Remove GitHub logging commands from start.cmd
    Removed GitHub logging commands from start.cmd.

--- start.cmd
+++ start.cmd
@@ -4,11 +4,6 @@ setlocal
 rem Atualiza o repositório
 git pull
 
-rem Adiciona logs e envia para o GitHub
-git add *.txt
-git commit -m "WINDOWS SERVER CRASH LOGS"
-git push origin main
-
 rem Executa o servidor e gera log único
 set "id=%random%"
 npm run start >> "%id%-WINDOWS-SERVER-%COMPUTERNAME%.txt" 2>&1
