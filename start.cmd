42eb880d0acefa30a790d65bde2abd2c6987dfe8
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-11-01 16:34:09 -0300

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,7 +1,14 @@
 @echo off
-git reset --hard HEAD
+setlocal
+
+rem Atualiza o repositório
 git pull
 
+rem Adiciona logs e envia para o GitHub
+git add *.txt
+git commit -m "WINDOWS SERVER CRASH LOGS"
+git push origin main
+
+rem Executa o servidor e gera log único
 set "id=%random%"
-npm run start >> %random%-WINDOWS-SERVER-%COMPUTERNAME%.txt 2>&1
-pause
+npm run start >> "%id%-WINDOWS-SERVER-%COMPUTERNAME%.txt" 2>&1
