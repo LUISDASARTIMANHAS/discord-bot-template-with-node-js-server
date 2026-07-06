3ae5be0ca34d4f3fdba1f4f70e995d961946fd7e
PENTIUM-SERVER <luisaugustodesouza785@gmail.com>
2026-07-06 11:17:17 -0300

    Your descriptive message here

--- dedicated-server.cmd
+++ dedicated-server.cmd
@@ -1,9 +1,11 @@
 @echo off
 setlocal
 
-rem Atualiza o repositório
+title=Atualiza o repositório
+@rem Atualiza o repositório
 git pull
 
-rem Executa o servidor e gera log único
+@rem Executa o servidor e gera log único
 set "id=%random%"
+title=%id%-WINDOWS-SERVER-%COMPUTERNAME%.txt
 npm run startDedicated >> "logs/%id%-WINDOWS-SERVER-%COMPUTERNAME%.txt" 2>&1
