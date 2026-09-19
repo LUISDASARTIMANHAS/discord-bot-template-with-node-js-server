963a2f779ade97b17adbe933abdfcb4c3ad8ac06
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-09-19 11:36:49 -0300

    updated versions

--- pages.js
+++ pages.js
@@ -1,18 +1,11 @@
 import express from "express";
 const router = express.Router();
-import os from "os"
 import {
-  landingPage,
   exposeFolders,
-  StatusDashboard,
-  logsDashboard
 } from "npm-package-nodejs-utils-lda";
 
 
 exposeFolders(router,"src");
-
-StatusDashboard(router);
-logsDashboard(router);
 
 router.get("/debugger", (req, res) => {
   console.log("SISTEMA <OBTER> <SITE>: " + req.url);
